93469919df1ff0e18e5ced4f5ac8782ed6cb76db
Deadshot338 <ryantohjh@gmail.com>
2025-02-06 10:37:04 +0800

    update

--- src/client.py
+++ src/client.py
@@ -4,5 +4,26 @@
 import quickfix as fix
 from quickfix import Application, SessionID, Message
 
-# class Client(Application):
+class Application(_Object):
+    def onCreate(self, sessionID):
+        return
+    
+    def onLogon(self, sessionID):
+        return
+    
+    def onLogout(self, sessionID):
+        return
+    
+    def toAdmin(self, message, sessionID):
+        return
+    
+    def fromAdmin(self, message, sessionID):
+        return
+    
+    def toApp(self, message, sessionID):
+        return
+    
+    def fromApp(self, message, sessionID):
+        return
+    
 
